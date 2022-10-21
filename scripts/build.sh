ab55bf6ff27b11b30261c1908e6bdbbc33275eef
Raphael Manke <raphael.manke@codecentric.de>
2022-10-21 10:44:01 +0200

    chore: :recycle: rename artifact name
    The old artifact name was from the example repo. It is now more specific to the actual purpose

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Building for architecture ${ARCH:-amd64}..."
-GOOS=linux GOARCH=${ARCH} go build -o bin/extensions/go-example-extension main.go
-chmod +x bin/extensions/go-example-extension
+GOOS=linux GOARCH=${ARCH} go build -o bin/extensions/secret-cache-extension main.go
+chmod +x bin/extensions/secret-cache-extension
 echo "Building finished."
